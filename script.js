d1e62a0a6998f5501e5e53b7c562dc29d336f105
balhyo-younjisang <yjs12180825@gmail.com>
2022-11-25 00:54:29 +0900

    아니 왜 다 완성하고 나서 컨펌을 주냐고

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const logo = document.getElementById('logo');
-const mainBanner = document.getElementById('main-banner');
+const mainbanner = document.getElementById('main-banner');
 
 window.onresize = function (event) {
     let innerWidth = window.innerWidth;
-    innerWidth >= 1000 ? logo.src = "./img/Logo2.png" : logo.src = "./img/Logo4.png";
-    innerWidth >= 1000 ? mainBanner.src = "./img/mainBanner.png" : mainBanner.src = "./img/mainBanner2.png"
+    innerWidth >= 1000 ? logo.src = "./img/Logo2.png" : logo.src = "./img/Logo1.png";
+    innerWidth >= 600 ? mainbanner.src = "./img/mainBanner.png" : mainbanner.src = "./img/mainBanner2.png";
 }
 
 $(document).ready(function ($) {
