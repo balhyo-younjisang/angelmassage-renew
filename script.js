f767dd337c0436fbba9f16e1e6acf16439314c89
balhyo-younjisang <yjs12180825@gmail.com>
2022-11-25 21:28:48 +0900

    done.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const logo = document.getElementById('logo');
 const mainbanner = document.getElementById('main-banner');
 
-window.onresize = function (event) {
-    let innerWidth = window.innerWidth;
-    if (innerWidth >= 1000) {
-        mainbanner.src = "./img/mainBanner1.png";
-        logo.src = "./img/Logo2.png"
-    } else if (innerWidth >= 600) {
-        mainbanner.src = "./img/mainBanner2.png";
-        logo.src = "./img/Logo1.png";
-    } else {
-        mainbanner.src = "./img/mainBanner3.png";
-        logo.src = "./img/Logo1.png";
-    }
-}
+// window.onresize = function (event) {
+//     let innerWidth = window.innerWidth;
+//     if (innerWidth >= 1000) {
+//         mainbanner.src = "./img/mainBanner1.png";
+//         logo.src = "./img/Logo2.png"
+//     } else if (innerWidth >= 600) {
+//         mainbanner.src = "./img/mainBanner2.png";
+//         logo.src = "./img/Logo1.png";
+//     } else {
+//         mainbanner.src = "./img/mainBanner3.png";
+//         logo.src = "./img/Logo1.png";
+//     }
+// }
 
 $(document).ready(function ($) {
     $(".scroll_move").click(function (event) {
